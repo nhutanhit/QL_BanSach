cf46b25eb82080550e2071d075950b953f03abd1
27dvk11 <27dvk11@gmail.com>
2019-04-06 15:52:59 +0700

    kiem tra don hang

--- checkdetail.php
+++ checkdetail.php
@@ -1,17 +1,51 @@
+<!-- <?php include_once("header.php"); ?>
+    <ul class="menu">
+        <li>
+            <a href="/ThucHanhPHP/LAB3/list_product.php">Danh sách sản phẩm</a>
+        </li>
+        <li>
+            <a href="/ThucHanhPHP/LAB3/add_product.php">Thêm sản phẩm</a>
+        </li>
+    </ul>
+<?php include_once("footer.php") ?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bán sách</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+    <link href="css/my.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<script type="text/javascript" src="js/noel.js"></script>
+</head>
+
 <body>
 <?php
      require_once("entities/product.class.php");
      require_once("entities/slide.class.php");
-     //sp
-    $prods =  Product :: list_product();
-    require_once('config/db.class.php');
-    $db2 = new Db();
-    // loại sp
-    $sql2 = "Select * from category";
-    $result1 = $db2->select_to_array($sql2);
-    // slide
-      $listSlide =  Slide :: listproduct();
+
+
 ?>
+
     <!-- Navigation -->
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
@@ -23,7 +57,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.php">Home</a>
+                <a class="navbar-brand" href="#">Home</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -37,15 +71,15 @@
                 </ul>
 
                 <form class="navbar-form navbar-left" role="search">
-			        <div class="form-group">
-			          <input type="text" class="form-control" placeholder="Search">
-			        </div>
-			        <button type="submit" class="btn btn-default">Submit</button>
-			    </form>
+    			        <div class="form-group">
+    			          <input type="text" class="form-control" placeholder="Search">
+    			        </div>
+    			        <button type="submit" class="btn btn-default">Submit</button>
+			           </form>
 
-			    <ul class="nav navbar-nav pull-right">
+			         <ul class="nav navbar-nav pull-right">
                     <li>
-                        <a href="#">Đăng ký</a>
+                        <a href="register.php">Đăng ký</a>
                     </li>
                     <li>
                         <a href="login.php">Đăng nhập</a>
@@ -53,7 +87,7 @@
                     <li>
                     	<a>
                     		<span class ="glyphicon glyphicon-user"></span>
-                    		KhanhDV
+                    		Bùi Đức Phú
                     	</a>
                     </li>
 
@@ -67,40 +101,81 @@
         </div>
         <!-- /.container -->
     </nav>
+
     <!-- Page Content -->
     <div class="container">
-    	<!-- slider -->
     	<div class="row carousel-holder">
-            <div class="col-md-12">
-            </div>
-        </div>
-        <!-- end slide -->
-        <div class="space20"></div>
-        <div class="row main-left">
-            <div class="col-md-9">
-                  <div class="container">
-                  <div class="row">
-                  <div class="col-md-4"></div>
-                  <div class="col-md-4" style="margin-top: 100px;">
-                      <form method="POST" action="register.php">
-                          <div class="form-group">
-                              <label>Mã đơn hàng</label>
-                              <input type="text" class="form-control" name="txtMadonhang" autofocus value="<?php echo !empty($_POST['txtMadonhang']) ? $_POST['txtMadonhang'] : ''; ?>" required>
-                          </div>
-                          <div class="form-group">
-                            <input class="btn btn-primary" type="submit" name="submit" value="Tìm kiếm">
-                          </div>
-                      </form>
+                <div class="row main-left">
+
+                    <div class="col-md-4"></div>
+                      <div class="col-md-4" style="margin-top: 100px;">
+                          <form method="POST" action="register.php" >
+                              <div class="form-group" >
+                                <label>Mã đơn hàng</label>
+                                  <input type="text" class="form-control" name="txtMadonhang" autofocus value="<?php echo !empty($_POST['txtMadonhang']) ? $_POST['txtMadonhang'] : ''; ?>" required>
+                              </div>
+                              <div class="form-group" style="float:right">
+                                  <input class="btn btn-primary" type="submit" name="submit" value="Tìm kiếm">
+                              </div>
+                          </form>
                       </div>
                       <div class="col-md-4"></div>
+
+                  <div class="col-md-12">
+                  <center>
+                    <!-- <div class="col-sm-8"> -->
+                      <h1>Thông tin đơn hàng</h1>
+                      <div class="row">
+                        <table class="table table-bordered sm-3" style="width:100%">
+                          <thead class="thead-dark">
+                            <tr>
+                              <th>Product ID</th>
+                                <th>Product Name</th>
+                                <th>Product Type</th>
+                                <th>Price</th>
+                                <th>Picture</th>
+                                <th>Cập nhật</th>
+                                <th>Xóa</th>
+                            </tr>
+                          </thead>
+                            <tr>
+                              <th>1</th>
+                                <th>Mập</th>
+                                <th>Địt</th>
+                                <th>1đ</th>
+                                <td><img src='uploads/".$item["Picture"]."' style='width:100px;height:100px'/></td>
+                                <td><a class='btn btn-warning' href='all_in_one.php?edit=true&ProductID=".$item["ProductID"]."'>Cập nhật</a></td>
+                                <td><a onclick='myFunction(".$item["ProductID"].");' class='btn btn-danger' >Xóa</a></td>
+                            </tr>
+                        </table>
+
+                        <table id="t01" border="1" style="width:25%">
+                        <tr>
+                          <th>Số lượng: </th>
+                          <th>?? </th>
+                            <th>Tổng tiền: </th>
+                            <th>??</th>
+                        </tr>
+                      </table>
+                      </div>
+                      <div class="row" style="margin-top: 30px"></div>
+                      <a class="btn btn-primary ml-2" href="#">Thanh toán</a>
+                      <a class="btn btn-primary ml-2" href="index.php">Quay lại</a>
+                    <!-- </div> -->
+                  </center>
                   </div>
-              </div>
-					</div>
+        				</div>
 	            </div>
         	</div>
         </div>
+        <!-- /.row -->
     </div>
+    <!-- end Page Content -->
+
+    <!-- Footer -->
     <hr>
+
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
--- config/db.class.php
+++ config/db.class.php
@@ -7,10 +7,10 @@
     <title>Document</title>
     </head>
 <body>
-    
 
 
-<?php 
+
+<?php
 
 class Db
 {
